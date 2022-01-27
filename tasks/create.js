224e065beba4f2e424cffcdb7687fe0f735f2a7b
Goncalo Santos <goncaloggsantos@gmail.com>
2022-01-27 16:24:52 +0000

    chore: script update

--- tasks/create.js
+++ tasks/create.js
@@ -1,14 +1,45 @@
 import chalk from 'chalk'
 import commander from 'commander'
-import prompts from 'prompts';
+import prompts from 'prompts'
+import fs from 'fs/promises'
+import path from 'path'
 
 // import packageJson from '../packages/minimal/package.json'
 
-let projectName
+async function checkDirExists (name) {
+  try {
+    await fs.access(name)
+    console.log('Directory already exists!')
+    process.exit(1)
+  } catch(err) {
+    return false
+  }
+}
 
-function changePackageName () {
-  
-} 
+async function createDir (name) {
+  try {
+    await fs.mkdir(name)
+    console.log('Directory created!')
+  } catch(err) {
+    console.log('Failed to create directory.')
+  }
+}
+
+async function copyContent (name) {
+  try {
+    const minimalPath = path.resolve('./packages/minimal')
+    const destinationPath = path.resolve(name)
+
+    const jsonFile = await fs.readFile(`${minimalPath}/package.json`)
+    const packageJson = JSON.parse(jsonFile) 
+    packageJson.name = name
+    
+    await fs.cp(minimalPath, destinationPath, { recursive: true })
+    await fs.writeFile(path.join(name, 'package.json'), JSON.stringify(packageJson, null, 2))
+  } catch(err) {
+    console.log(err)
+  }
+}
 
 async function getName () {
   const response = await prompts({
@@ -35,7 +66,14 @@ async function getPlugins () {
 
 async function init () {
   const name = await getName()  
-  const plugins = await getPlugins()
+  //  const plugins = await getPlugins()
+  const dirExists = await checkDirExists(name)
+
+  if (!dirExists) await createDir(name)
+
+  await copyContent(name)
+
 }
+
 
 init()
